b2b59b3cfcfbc7edcb6b18705c61336394e0f797
Paul <paul.uhn@gmail.com>
2024-03-10 18:08:13 -0500

    Fix jumping bugs

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,9 +5,13 @@ extends CharacterBody2D
 @export var gravity = 1 # this is the power of gravity in our game
 @export var jump_power = 1 # this is power of jumping for my character
 
+@onready var sprite = $AnimatedSprite2D
+@onready var jump_sound = $JumpSound
+
 var active = true # vocabulary word: boolean - true or false
 var jump_remaining = 2 # right now 2 means double jump, if you want triple jump make it a 3
 var was_jumping = false
+var jump_pitch = 1.0
 
 func _ready():
 	print("hello nathans")
@@ -16,10 +20,27 @@ func _physics_process(delta):
 	velocity.y += gravity * delta
 	
 	if active:
+		# Reset the player after landing on platform
+		if was_jumping and is_on_floor():
+			was_jumping = false
+			jump_remaining = 2
+			sprite.play("run")
+			jump_pitch = 1.0
+		
 		# Handle jumping
 		if Input.is_action_just_pressed("jump") and jump_remaining > 0:
 			jump_remaining -= 1 # j = j - 1
 			was_jumping = true
 			velocity.y = -jump_power
+	
+			if jump_remaining == 1:
+				sprite.play("jump")
+			else:
+				sprite.play("double jump")
+				
+			jump_sound.set_pitch_scale(jump_pitch)
+			jump_sound.play()
+			jump_pitch += 0.2
+	
 	
 	move_and_slide()
