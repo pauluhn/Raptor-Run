f9a510cb3a25d02d430f70dc87971aa4ddf3d6cd
Paul <paul.uhn@gmail.com>
2024-04-06 18:13:53 -0500

    save from two weeks ago

--- scripts/game.gd
+++ scripts/game.gd
@@ -3,11 +3,31 @@ extends Node2D
 @export var world_speed = 1
 @export var moving_environment: Node2D
 
+var platform = preload("res://scenes/platform.tscn")
 var rng = RandomNumberGenerator.new()
+var last_platform_position = Vector2.ZERO
+var next_spawn_time = 0
 
 func _ready():
 	rng.randomize()
 	
+func _process(delta):
+	# Check to see if we need to spawn a platform
+	if Time.get_ticks_msec() > next_spawn_time:
+		var new_platform = platform.instantiate()
+		
+		# Set the position of the new platform
+		if last_platform_position == Vector2.ZERO:
+			new_platform.position = Vector2(400, 0)
+		else:
+			var x = last_platform_position.x + rng.randi_range(450, 550)
+			var y = last_platform_position.y + rng.randi_range(-150, 150)
+			new_platform.position = Vector2(x, y)
+			
+		moving_environment.add_child(new_platform)
+		last_platform_position = new_platform.position
+		next_spawn_time += world_speed
+
 
 func _physics_process(delta):
 	# Move the platforms inside the Moving Environment
