b72824f6cac1ef0d62a72e30fb565b0b69b054b8
Paul <paul.uhn@gmail.com>
2024-04-13 17:14:59 -0500

    refactor

--- scripts/game.gd
+++ scripts/game.gd
@@ -14,20 +14,22 @@ func _ready():
 func _process(delta):
 	# Check to see if we need to spawn a platform
 	if Time.get_ticks_msec() > next_spawn_time:
-		var new_platform = platform.instantiate()
-		
-		# Set the position of the new platform
-		if last_platform_position == Vector2.ZERO:
-			new_platform.position = Vector2(400, 0)
-		else:
-			var x = last_platform_position.x + rng.randi_range(450, 550)
-			var y = last_platform_position.y + rng.randi_range(-150, 150)
-			new_platform.position = Vector2(x, y)
-			
-		moving_environment.add_child(new_platform)
-		last_platform_position = new_platform.position
-		next_spawn_time += world_speed
+		_spawn_next_platform() # call our new method that we created to refactor our code to be cleaner
 
+func _spawn_next_platform():
+	var new_platform = platform.instantiate()
+		
+	# Set the position of the new platform
+	if last_platform_position == Vector2.ZERO:
+		new_platform.position = Vector2(400, 0)
+	else:
+		var x = last_platform_position.x + rng.randi_range(450, 550)
+		var y = last_platform_position.y + rng.randi_range(-150, 150)
+		new_platform.position = Vector2(x, y)
+		
+	moving_environment.add_child(new_platform)
+	last_platform_position = new_platform.position
+	next_spawn_time += world_speed
 
 func _physics_process(delta):
 	# Move the platforms inside the Moving Environment
